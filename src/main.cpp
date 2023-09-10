28895def7ab50a850908416cabb76a51b56dcf46
julian long <julianlong@163.com>
2023-09-10 10:04:50 +0800

    one dimension array study

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    /***
+     * array
+     
+    int myarray[5] = {0};
+
+    for (unsigned int i = 0; i < 5; i++)
+    {
+        myarray[i] = i;
+    }
+
+    for (unsigned j = 0; j < 5; j++)
+    {
+        cout << *(&myarray[0] + j) << endl;
+    }
+    */
+
+    cout << "my program starting" << endl;
+    return 0;
+}
